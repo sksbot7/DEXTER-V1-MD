8e3f033aaf50af0866ca1a1a6300a5d03cf93f7f
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-05-08 12:02:01 +0530

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -7,7 +7,7 @@ const { default: axios } = require('axios');
 zokou({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🚨', 
+    reaction: '😹', 
     fromMe: 'true', 
 
        
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('📌 *DEXTER-MD ♨ PING*\n ```' + 2000 + '``` *ms*') 
+    return repondre('📌 *@_sks_bot_ 🐦 OOMB*\n ```' + 999 + '``` *ms*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
