7f1077aa9e8039a5d2ab7adc4ed1369ab8c9e2b5
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-05-08 12:06:04 +0530

    Update alive.js

--- commandes/alive.js
+++ commandes/alive.js
@@ -73,8 +73,8 @@ else {
     } else {
         if(!superUser) { repondre("there is no alive for this bot") ; return};
 
-      await   repondre("⚔ 𝐃𝐄𝐗𝐓𝐄𝐑 𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓 𝗢𝗪𝗡𝗘𝗥 𝗗𝗘𝗫𝗧𝗘𝗥 ⚔");
-         repondre(" *Support ravi and chaky ! cyber dexter")
+      await   repondre("NJAN ALIVE AAN😌😹");
+         repondre(" *contact: ig| _sks_bot_")
      }
  } else {
 
